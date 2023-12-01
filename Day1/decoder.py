8540be4ec78833f813a7702bfcb7d281768dfbea
benstream <benjaminstream@yahoo.com>
2023-12-01 00:56:01 -0900

    Setting up for problem B

--- Day1/decoder.py
+++ Day1/decoder.py
@@ -9,11 +9,35 @@
 # In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 # Consider your entire calibration document. What is the sum of all of the calibration values?
 
+# OG VERSION! Doesnt cound spelled out numbers as numbers! Now we need to...
+# sumdigits = 0
+# with open(".\Day1\input.txt","r") as txt:
+#     for line in txt:
+#        digits = list(filter(lambda i: i.isdigit(), line))
+#        sumdigits += int(str(digits[0])+str(digits[-1]))
+#     print("Your sum is: " + str(sumdigits))
+
 sumdigits = 0
+numdict = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0'
+}
 with open(".\Day1\input.txt","r") as txt:
     for line in txt:
+       while True:
+        try:
+            break
+        except ValueError:
+            pass   
        digits = list(filter(lambda i: i.isdigit(), line))
-       sumdigits += int(str(digits[0])+str(digits[-1]))
+       sumdigits += int(str(digits[0])+str(digits[-1][-1]))
     print("Your sum is: " + str(sumdigits))
-
        
